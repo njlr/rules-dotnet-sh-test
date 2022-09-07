8fee6afca2337feede3450eb42094e989322a3e2
njlr <njlr@users.noreply.github.com>
2022-09-08 00:00:44 +0100

    * Docker experiment

--- app/Program.fs
+++ app/Program.fs
@@ -1,4 +1,10 @@
-﻿[<EntryPoint>]
+﻿open System
+open System.Threading
+open System.Threading.Tasks
+open Docker.DotNet
+open Docker.DotNet.Models
+
+[<EntryPoint>]
 let main argv =
   let password =
     argv
@@ -7,6 +13,57 @@ let main argv =
 
   if password = "abcdef" then
     printfn "Correct password"
+
+    task {
+      printfn "Starting Docker..."
+
+      use client = (new DockerClientConfiguration()).CreateClient()
+
+      printfn "Creating image..."
+
+      let icps = ImagesCreateParameters()
+
+      icps.FromImage <- "library/hello-world"
+
+      do!
+        client.Images.CreateImageAsync(
+          icps,
+          AuthConfig(),
+          Progress<JSONMessage>())
+
+      printfn "Creating container..."
+
+      let ccps = CreateContainerParameters()
+
+      ccps.Image <- "library/hello-world"
+
+      let! ccr = client.Containers.CreateContainerAsync(ccps)
+
+      printfn $"{ccr.ID}"
+
+      printfn "Starting container..."
+
+      let! wasStarted =
+        client.Containers.StartContainerAsync(
+          ccr.ID,
+          ContainerStartParameters());
+
+      printfn $"{wasStarted}"
+
+      printfn "Stopping container..."
+
+      let! wasStopped =
+        client.Containers.StopContainerAsync(
+          ccr.ID,
+          ContainerStopParameters(),
+          CancellationToken.None)
+
+      printfn $"{wasStopped}"
+
+      printfn "Done."
+    }
+    |> fun t -> t.Result
+
     0
   else
     printfn "Incorrect password"
